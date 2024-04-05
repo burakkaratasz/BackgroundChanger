75e71d89ed8ee7e8f196e78066ffbb0df3da0b98
burakkaratasz <burakkaratasz@outlook.com>
2024-04-05 16:50:44 +0300

    ImageModeFragment added

--- app/src/main/java/com/example/backgroundchanger/fragment/ImageModeFragment.kt
+++ app/src/main/java/com/example/backgroundchanger/fragment/ImageModeFragment.kt
@@ -1,60 +1,104 @@
 package com.example.backgroundchanger.fragment
 
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.graphics.drawable.toBitmap
 import com.example.backgroundchanger.R
+import com.example.backgroundchanger.databinding.FragmentImageModeBinding
+import com.example.backgroundchanger.utils.GroundInstance
+import com.example.backgroundchanger.utils.ImageUtils
+import com.example.backgroundchanger.utils.LoadingDialog
+import com.example.backgroundchanger.utils.PermissionCheckerUtils
+import com.google.android.material.chip.Chip
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ImageModeFragment : Fragment(), OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ImageModeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ImageModeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentImageModeBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var loading: LoadingDialog
+    private lateinit var chip: Chip
+    private lateinit var buttonSegment : Button
+    private lateinit var buttonCamera : FloatingActionButton
+    private lateinit var buttonAddGallery : FloatingActionButton
+    private lateinit var buttonBackup : FloatingActionButton
+    private lateinit var buttonDownload : FloatingActionButton
+    private lateinit var imageViewUri : Uri
+    private lateinit var selectedBitmap : Bitmap
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_mode, container, false)
-    }
+    ): View {
+        _binding = FragmentImageModeBinding.inflate(inflater, container, false)
+        loading = LoadingDialog(requireActivity(), inflater)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ImageModeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ImageModeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        binding.apply {
+            chip.isChecked = true
+            chip.setOnCheckedChangeListener { button, isChecked ->
+                if (isChecked) {
+                    button.text = "Foreground Mode"
+                    mode = !mode
+                }
+                else {
+                    button.text = "Background Mode"
+                    mode = !mode
                 }
             }
+        }
+        buttonSegment.setOnClickListener(this@ImageModeFragment)
+        buttonCamera.setOnClickListener(this@ImageModeFragment)
+        buttonBackup.setOnClickListener(this@ImageModeFragment)
+        buttonDownload.setOnClickListener(this@ImageModeFragment)
+        buttonAddGallery.setOnClickListener(this@ImageModeFragment)
+
+        return binding.root
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.buttonSegment -> {
+                loading.show()
+                ImageAnalyzer().analyze(binding.imageView.drawable.toBitmap())
+                Handler(Looper.getMainLooper()).postDelayed({
+                   binding.imageView.setImageBitmap(GroundInstance.getInstance()?.foregroundImage)
+                }, 2000)
+            }
+            R.id.buttonCamera -> {
+                PermissionCheckerUtils.checkCameraPerm(requireContext(), requireActivity()) {
+                    imageViewUri = ImageUtils.getImageUriFromBitmap(requireContext(), binding.imageView.drawable.toBitmap())
+                }
+            }
+            R.id.buttonAddGallery -> {
+                PermissionCheckerUtils.checkGalleryReadPerm(requireContext(), requireActivity()) {
+
+                }
+            }
+            R.id.buttonBackup -> {
+                binding.imageView.setImageResource(0)
+                binding.imageView.setImageBitmap(GroundInstance.getInstance()?.backupImage)
+            }
+            R.id.buttonDownload -> {
+                PermissionCheckerUtils.checkGalleryWritePerm(requireContext(), requireActivity()) {
+                    ImageUtils.download(binding.imageView.drawable.toBitmap(), requireContext())
+                }
+            }
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    companion object {
+        var mode: Boolean = true
     }
 }
