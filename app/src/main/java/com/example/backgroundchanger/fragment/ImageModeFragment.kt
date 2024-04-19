f5129866deacb501265c17c26e55613a120ed120
burakkaratasz <burakkaratasz@outlook.com>
2024-04-19 14:59:42 +0300

    ImageModeFragment, from camera and gallery image selection added

--- app/src/main/java/com/example/backgroundchanger/fragment/ImageModeFragment.kt
+++ app/src/main/java/com/example/backgroundchanger/fragment/ImageModeFragment.kt
@@ -1,17 +1,22 @@
 package com.example.backgroundchanger.fragment
 
 import android.graphics.Bitmap
+import android.media.Image
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.graphics.drawable.toBitmap
+import androidx.core.os.postDelayed
 import com.example.backgroundchanger.R
 import com.example.backgroundchanger.databinding.FragmentImageModeBinding
 import com.example.backgroundchanger.utils.GroundInstance
@@ -76,11 +81,12 @@ class ImageModeFragment : Fragment(), OnClickListener {
             R.id.buttonCamera -> {
                 PermissionCheckerUtils.checkCameraPerm(requireContext(), requireActivity()) {
                     imageViewUri = ImageUtils.getImageUriFromBitmap(requireContext(), binding.imageView.drawable.toBitmap())
+                    takePhotoResult.launch(imageViewUri)
                 }
             }
             R.id.buttonAddGallery -> {
                 PermissionCheckerUtils.checkGalleryReadPerm(requireContext(), requireActivity()) {
-
+                    selectImageFromGallery()
                 }
             }
             R.id.buttonBackup -> {
@@ -92,6 +98,67 @@ class ImageModeFragment : Fragment(), OnClickListener {
                     ImageUtils.download(binding.imageView.drawable.toBitmap(), requireContext())
                 }
             }
+        }
+    }
+
+    //camera
+    private val takePhotoResult = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+        if (it) {
+            imageViewUri.let {
+                uri ->
+                if (mode) {
+                    binding.imageView.setImageResource(0)
+                    binding.imageView.setImageURI(uri)
+                    GroundInstance.getInstance()?.backupImage = binding.imageView.drawable.toBitmap()
+                    binding.buttonBackup.visibility = View.VISIBLE
+                } else {
+                    loading.show()
+                    binding.imageView.setImageResource(0)
+                    createSelfieWithBackground(uri)
+                }
+            }
+        }
+    }
+
+    //gallery
+    private fun selectImageFromGallery() = selectImageFromGalleryResult.launch("image/*")
+    private val selectImageFromGalleryResult = registerForActivityResult(ActivityResultContracts.GetContent()) {uri ->
+        uri?.let {
+            if (mode) {
+                binding.imageView.setImageResource(0)
+                binding.imageView.setImageURI(uri)
+                GroundInstance.getInstance()?.backupImage = binding.imageView.drawable.toBitmap()
+                binding.buttonBackup.visibility = View.VISIBLE
+            } else {
+                loading.show()
+                createSelfieWithBackground(uri)
+            }
+        }
+    }
+    private fun createSelfieWithBackground(uri: Uri?) {
+        if(GroundInstance.getInstance()?.foregroundImage != null) {
+            binding.imageView.setImageResource(0)
+            selectedBitmap = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, uri)
+
+            val resizedBitmap = ImageUtils.resizeBitmap(
+                selectedBitmap,
+                GroundInstance.getInstance()?.foregroundImage!!.width,
+                GroundInstance.getInstance()?.foregroundImage!!.height
+            )
+
+            ImageAnalyzer().analyzeWithBG(
+                GroundInstance.getInstance()?.foregroundImage!!,
+                resizedBitmap
+            )
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                binding.imageView.setImageBitmap(GroundInstance.getInstance()?.backgroundImage)
+            }, 2000)
+
+        }
+        else {
+            Toast.makeText(requireContext(), "Make a segment selfie before choosing a background!", Toast.LENGTH_SHORT).show()
+            loading.dismiss()
         }
     }
     override fun onDestroyView() {
